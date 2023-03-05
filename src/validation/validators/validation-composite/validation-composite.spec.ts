99e89261aeb2b20480440cf2bad9e98d6a1162a7
ItaloRez <italo.rezende@sharkdev.com.br>
2023-03-05 17:01:48 -0300

    test: ensure validationComposite returs the first error found

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,14 @@
+import { FieldValidationSpy } from "@/validation/test/mock-fieldValidation";
+import { ValidationComposite } from "./validation-composite";
+
+describe("ValidationComposite", () => {
+  test("Should return error if any validation fails", () => {
+    const validationSpy = new FieldValidationSpy("any_field");
+    validationSpy.error = new Error("first_error_message");
+    const validationSpy2 = new FieldValidationSpy("any_field");
+    validationSpy2.error = new Error("second_error_message");
+    const sut = new ValidationComposite([validationSpy, validationSpy2]);
+    const error = sut.validate("any_field", "any_value");
+    expect(error).toBe("first_error_message");
+  });
+});
