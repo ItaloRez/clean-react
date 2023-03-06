d1c23cadc38a1c5170e3ab246d4757fe8729a6c5
ItaloRez <italo.rezende@sharkdev.com.br>
2023-03-05 21:15:02 -0300

    feat: add login composition

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,13 +1,17 @@
 import { Login } from "@/presentation/pages";
-import React from "react";
+import React, { ReactNode } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const Router: React.FC = () => {
+type Props = {
+  MakeLogin: React.FC;
+};
+
+const Router: React.FC<Props> = ({ MakeLogin }: Props) => {
   const router = createBrowserRouter([
-    // {
-    //   path: "/login",
-    //   element: <Login />,
-    // },
+    {
+      path: "/login",
+      element: <MakeLogin />,
+    },
   ]);
 
   return <RouterProvider router={router} />;
